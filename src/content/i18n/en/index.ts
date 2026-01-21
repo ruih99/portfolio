68cb1f51d36554c5558eb1b951f629752c03383c
ruih99 <hndrui.9942@gmail.com>
2026-01-21 11:33:46 +0900

    i18n英語コンテンツ追加

---
+++ src/content/i18n/en/index.ts
@@ -0,0 +1,19 @@
+import portfolio from '../shared/portfolio.json';
+import tabs from './tabs.json';
+import skills from '../shared/skills.json';
+import careers from './careers.json';
+import profileTab from './profileTab.json';
+import social from '../shared/social.json';
+import projects from './projects.json';
+
+export const en = {
+  portfolio,
+  tabs,
+  skills,
+  careers,
+  profileTab,
+  social,
+  projects,
+};
+
+export default en;
